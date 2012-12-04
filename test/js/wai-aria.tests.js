5c6868ea7c22e853089fb24d76a4937175855c9c
Yura Zenevich <yura.zenevich@gmail.com>
2012-12-04 08:49:12 -0500

    Working tests for both node and browser.

---
+++ test/js/wai-aria.tests.js
@@ -0,0 +1,33 @@
+/*global module, describe:true, it:true, expect:true*/
+(function (module) {
+
+    "use strict";
+
+    module.exports = function (wa11y, expect) {
+        describe("wai-aria", function () {
+            it("Rule apply", function (done) {
+                // TODO: expect(1)
+                var testValidator = wa11y.init();
+                testValidator.configure({
+                    rules: {
+                        "wai-aria": {}
+                    }
+                });
+                testValidator.on("complete", function (log) {
+                    var key, docId, thisLog;
+                    for (key in log) {
+                        for (docId in log[key]) {
+                            thisLog = log[key][docId];
+                            expect(thisLog).to.deep.equal({
+                                INFO: ["wai-aria test passed."]
+                            });
+                        }
+                    }
+                    done();
+                });
+                testValidator.run('<p><a class="the-link" href="https://github.com/yzen/wa11y">wa11y\'s Homepage</a></p>');
+            })
+        });
+    };
+
+})(module);
--- test/wai-aria.tests.js
+++ test/wai-aria.tests.js
@@ -1,32 +1,4 @@
-/* global wa11y */
-(function (wa11y, expect) {
-
-    describe("wai-aria", function () {
-        it("Rule apply", function (done) {
-            // TODO: expect(1)
-            var testValidator = wa11y.init();
-            testValidator.configure({
-                rules: {
-                    "wai-aria": {}
-                }
-            });
-            testValidator.on("complete", function (log) {
-                var key, docId, thisLog;
-                for (key in log) {
-                    for (docId in log[key]) {
-                        thisLog = log[key][docId];
-                        expect(thisLog).to.deep.equal({
-                            INFO: ["wai-aria test passed."]
-                        });
-                    }
-                }
-            });
-            testValidator.run('<p><a class="the-link" href="https://github.com/yzen/wa11y">wa11y\'s Homepage</a></p>');
-            done();
-        })
-    });
-
-})(
-    typeof wa11y === "undefined" ? require("../wa11y.js") : wa11y,
-    typeof expect === "undefined" ? require("chai").expect : expect
-);
+var wa11y = require("../wa11y.js"),
+    expect = require("chai").expect;
+require("../rules/wai-aria.js")(wa11y);
+require("./js/wai-aria.tests.js")(wa11y, expect);
