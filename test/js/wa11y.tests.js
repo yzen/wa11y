ff5a59cc4aa945f2d1050c560b415f7017d1edf8
Alexey Novak <abovebluesky@gmail.com>
2012-12-04 20:42:38 -0500

    Added a test function for engine functionality inside the rule

--- test/js/wa11y.tests.js
+++ test/js/wa11y.tests.js
@@ -48,6 +48,16 @@
                     } else {
                         this.complete(passReport);
                     }
+                },
+                ruleWithEngine = function (src) {
+                    var engine = this.engine,
+                        link = engine.find(".the-link");
+                    
+                    if (link.length < 1) {
+                        this.complete(failReport);
+                    } else {
+                        this.complete(passReport);
+                    } 
                 };
     
             wa11y.register({
@@ -66,6 +76,10 @@
                 name: "syncRuleRevert",
                 description: "Test synchronous rule revert",
                 rule: syncRuleRevert
+            }).register({
+                name: "ruleWithEngine",
+                description: "Test rule with engine inside",
+                rule: ruleWithEngine
             });
             
             it("wa11y.isHTML", function() {
@@ -492,6 +506,28 @@
                         done();
                     });
                 });
+                
+                it("engine functions inside the rule", function (done) {
+                    // TODO: expect(1)
+                    var testValidator = wa11y.init();
+                    testValidator.configure({
+                        rules: {
+                            ruleWithEngine: {}
+                        }
+                    });
+                    testValidator.on("complete", function (log) {
+                        var key, docId, thisLog;
+                        for (key in log) {
+                            for (docId in log[key]) {
+                                thisLog = log[key][docId];
+                                expect(thisLog).to.deep.equal(expectedPassReport);
+                            }
+                        }
+                        done();
+                    });
+                    testValidator.run(simpleSource);
+                });
+                
             });
         });
     };
